0d976c9a80a3f3e2a2a9e6df69b5907064b4861e
Rodrigo Andrés Costa <c.rodri92@gmail.com>
2022-04-21 20:51:54 -0300

    Update ReflectionExample.java

--- src/main/java/reflection/ReflectionExample.java
+++ src/main/java/reflection/ReflectionExample.java
@@ -1,16 +1,22 @@
 package reflection;
 
+import hospital.models.staff.Doctor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 
 import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class ReflectionExample {
 
     private static final Logger LOGGER = LogManager.getLogger(ReflectionExample.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
 
         // Create a new logger context (just testing this method)
         LoggerContext context = (LoggerContext) LogManager.getContext(true);
@@ -19,5 +25,24 @@ public class ReflectionExample {
 
         LOGGER.info("Starting application");
 
+        //Get class constructors
+        Constructor[] constructor = Doctor.class.getDeclaredConstructors();
+
+
+        //Get class fields
+        Field[] fields = Doctor.class.getDeclaredFields();
+        LOGGER.info("Fields: " + Arrays.stream(fields).toList());
+
+        //Get all the class methods
+        Method[] methods = Doctor.class.getDeclaredMethods();
+        LOGGER.info("Methods: " + Arrays.stream(methods).toList());
+
+        try {
+            Doctor doc = (Doctor) constructor[0].newInstance();
+
+            LOGGER.info("Created new doctor: " + doc.toString());
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            LOGGER.error(e);
+        }
     }
 }
